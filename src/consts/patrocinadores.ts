7c2c3636209f5a1d3d2874454535a9c640bbf7e1
Adriandpdev <adri.2000@hotmail.com>
2024-03-19 12:13:51 +0100

    add patrocinadores

--- src/consts/patrocinadores.ts
+++ src/consts/patrocinadores.ts
@@ -1,9 +1,21 @@
 type PatrocinadorId =
-	| "alsa1"
-	| "alsa2"
-	| "alsa3"
+	| "Sercine"
+	| "Duran"
+	| "Osdama"
+	| "Casaserra"
+	| "Albia"
+	| "Fantasmas"
+	| "LiliCook"
+	| "Guinaldo"
 type PatrocinadorName =
-	| "Alsa"
+	| "Sercine"
+	| "Plasticos Duran"
+	| "Osdama"
+	| "Casaserra"
+	| "Grupo Albia"
+	| "Fantasmas en el desvan"
+	| "LiliCook Gastrobar"
+	| "Restaurante Guinaldo"
 
 interface Patrocinador {
 	id: PatrocinadorId
@@ -13,34 +25,88 @@ interface Patrocinador {
 		width: number
 		height: number
 	}
+	svg: boolean
 }
 
 export const PatrocinadoresList: Array<Patrocinador> = [
 	{
-		id: "alsa1",
-		name: "Alsa",
-		url: "https://alsa.es/",
+		id: "Sercine",
+		name: "Sercine",
+		url: "https://sercine.com/",
 		image: {
 			width: 133,
 			height: 38,
 		},
+		svg: false
 	},
 	{
-		id: "alsa2",
-		name: "Alsa",
-		url: "https://alsa.es/",
+		id: "Duran",
+		name: "Plasticos Duran",
+		url: "https://www.plasticosduran.com/",
 		image: {
 			width: 133,
 			height: 38,
 		},
+		svg: false
 	},
 	{
-		id: "alsa3",
-		name: "Alsa",
-		url: "https://alsa.es/",
+		id: "Osdama",
+		name: "Osdama",
+		url: "https://www.instagram.com/osdama_salamanca/",
 		image: {
 			width: 133,
 			height: 38,
 		},
+		svg: false
+	},
+	{
+		id: "Casaserra",
+		name: "Casaserra",
+		url: "http://www.restaurantecasaserra.es/",
+		image: {
+			width: 133,
+			height: 38,
+		},
+		svg: false
+	},
+	{
+		id: "Albia",
+		name: "Grupo Albia",
+		url: "http://www.albia.es/",
+		image: {
+			width: 133,
+			height: 38,
+		},
+		svg: false
+	},
+	{
+		id: "Fantasmas",
+		name: "Fantasmas en el desvan",
+		url: "https://fantasmaseneldesvan.com/",
+		image: {
+			width: 133,
+			height: 38,
+		},
+		svg: false
+	},
+	{
+		id: "LiliCook",
+		name: "LiliCook Gastrobar",
+		url: "https://www.lilicook.es/",
+		image: {
+			width: 133,
+			height: 38,
+		},
+		svg: false
+	},
+	{
+		id: "Guinaldo",
+		name: "Restaurante Guinaldo",
+		url: "https://restauranteguinaldo.com/",
+		image: {
+			width: 133,
+			height: 38,
+		},
+		svg: false
 	}
 ] as const
