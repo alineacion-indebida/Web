6aa981cfea4a384d169ca75ccb1301563587b31f
Adriandpdev <adri.2000@hotmail.com>
2024-04-04 11:00:09 +0200

    fix order rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,11 +3,12 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
     const posts = await getCollection('noticias');
+    const formattedPosts = posts.sort((a, b) => new Date(b.data.date.valueOf()).getTime() - new Date(a.data.date.valueOf()).getTime())
     return rss({
         title: "Alineación indebida - El programa del deporte salmantino",
         description: "Web Oficial del programa Alineación Indebida. El programa de Unionistas de Salamanca y todo el deporte salmantino",
         site: context.site,
-        items: posts.map((post) => ({
+        items: formattedPosts.map((post) => ({
             title: post.data.title,
             pubDate: post.data.date,
             description: post.data.description,
