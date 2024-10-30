d8417a611a0bd4cb141d1f742173bf9a7c8f0c4d
Adriandpdev <adri.2000@hotmail.com>
2024-10-30 10:11:55 +0100

    add proximo partido

--- src/data/partidos.ts
+++ src/data/partidos.ts
@@ -2,5 +2,6 @@ import partidosData from "./partidos.json"
 
 const partidos = partidosData.partidos
 const partidosMap = new Map(partidos.map(partido => [partido.slug, partido]))
+const partidosNext = partidos.filter(partido => new Date(partido.date) > new Date())
 
-export { partidos, partidosMap }
+export { partidos, partidosMap, partidosNext }
